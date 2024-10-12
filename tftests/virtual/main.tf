a0da79ddd1f525a2f15831c05a14477e872decfd
Arthur Dayton <arthur@observeinc.com>
2024-10-12 15:51:39 -0700

    feat: add vistual datasets
    - Add ec2 virtual dataset
    - Add s3 virtual dataset
    - Add eks virtual dataset
    - Add lambda virtual dataset

---
+++ tftests/virtual/main.tf
@@ -0,0 +1,18 @@
+resource "random_pet" "test" {}
+
+data "observe_workspace" "default" {
+  name = "Default"
+}
+
+data "observe_datastream" "default" {
+  workspace = data.observe_workspace.default.oid
+  name      = "System"
+}
+
+module "default" {
+  source           = "../.."
+  workspace        = data.observe_workspace.default
+  name_format      = "test_${random_pet.test.id}/%s"
+  datastream       = data.observe_datastream.default
+  virtual_datasets = true
+}
