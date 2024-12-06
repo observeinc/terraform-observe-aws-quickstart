ae43c6251b6fe942edb2c374a320e69da26a10b0
Viraj Ramakrishnan <viraj.ramakrishnan@observeinc.com>
2024-12-05 16:09:51 -0800

    fix: add delimiter to bucket names and replace region map with cloud info

--- sources/s3forwarder/main.tf
+++ sources/s3forwarder/main.tf
@@ -6,21 +6,6 @@ locals {
   destination      = observe_filedrop.aws_filedrop.endpoint[0].s3[0]
   access_point_arn = observe_filedrop.aws_filedrop.endpoint[0].s3[0].arn
 
-  # the frontend gets the keys, which are the cluster names from 
-  # ops/kubernetes/clusters/{CLUSTER_NAME}/vars/shared-domain.env
-  cluster_region_map = {
-    eng         = "us-west-2"
-    o2          = "us-west-2"
-    prod        = "us-west-2"
-    sandbox     = "us-west-2"
-    staging     = "us-west-2"
-    prod-cap1-2 = "us-west-2"
-    prod-ap-1   = "ap-northeast-1"
-    prod-cap1   = "us-east-1"
-    prod-eu-1   = "eu-central-1"
-    prod-cba-1  = "ap-southeast-2"
-  }
-
   post_install_vars = {
     quick_create_link         = local.quick_create_link
     name                      = local.name
@@ -34,7 +19,7 @@ locals {
   cloudformation_parameters = {
     DataAccessPointArn = local.access_point_arn
     DestinationUri     = "s3://${local.destination.bucket}/${local.destination.prefix}"
-    SourceBucketNames  = join("", var.source_bucket_names)
+    SourceBucketNames  = join(",", var.source_bucket_names)
   }
 
   quick_create_link = join("", [for line in split("\n", <<-EOF
@@ -48,6 +33,8 @@ locals {
   ) : trimspace(line)])
 }
 
+data "observe_cloud_info" "this" {}
+
 resource "observe_filedrop" "aws_filedrop" {
   workspace  = var.workspace.oid
   datastream = var.datastream.oid
@@ -57,7 +44,7 @@ resource "observe_filedrop" "aws_filedrop" {
   config {
     provider {
       aws {
-        region   = lookup(local.cluster_region_map, var.observe_cluster, "us-west-2")
+        region   = data.observe_cloud_info.this.region
         role_arn = local.aws_role_arn
       }
     }
